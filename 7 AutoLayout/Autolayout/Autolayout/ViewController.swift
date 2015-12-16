b72341f09914a8d8fd4d69eef85da1a390ac651d
kevinzhow <kevinchou.c@gmail.com>
2015-12-16 21:38:01 +0800

    Init

---
+++ 7 AutoLayout/Autolayout/Autolayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Autolayout
+//
+//  Created by zhowkevin on 15/11/20.
+//  Copyright © 2015年 zhowkevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 8 TableView/TableViewFun/TableViewFun/NormalViewController.swift
@@ -0,0 +1,40 @@
+//
+//  NormalViewController.swift
+//  TableViewFun
+//
+//  Created by zhowkevin on 15/12/6.
+//  Copyright © 2015年 zhowkevin. All rights reserved.
+//
+
+import UIKit
+
+class NormalViewController: UIViewController {
+
+    @IBOutlet weak var normalLabel: UILabel!
+    
+    var normalText: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        normalLabel.text = normalText
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
